2a31a603e6da491fc26d95de2351dca9a7c72072
AnastasiyaKazantseva <anteleshko@yandex.ru>
2018-06-12 16:46:53 +0300

    module3-task1
    Исправление ошибок

--- js/map.js
+++ js/map.js
@@ -1,12 +1,45 @@
 'use strict';
 
-var AD_TITLES = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
-var AD_TYPES = ['palace', 'flat', 'house', 'bungalo'];
-var AD_TIMES = ['12:00', '13:00', '14:00'];
-var AD_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-var AD_PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+var AD_TITLES = [
+  'Большая уютная квартира',
+  'Маленькая неуютная квартира',
+  'Огромный прекрасный дворец',
+  'Маленький ужасный дворец',
+  'Красивый гостевой домик',
+  'Некрасивый негостеприимный домик',
+  'Уютное бунгало далеко от моря',
+  'Неуютное бунгало по колено в воде'
+];
+
+var AD_TYPES = [
+  'palace',
+  'flat',
+  'house',
+  'bungalo'
+];
+
+var AD_TIMES = [
+  '12:00',
+  '13:00',
+  '14:00'
+];
+
+var AD_FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner'
+];
+
+var AD_PHOTOS = [
+  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
+];
+
 var mapToggle = document.querySelector('.map');
-var similarAds = getAds();
 var pinListElement = document.querySelector('.map__pins');
 var pinTemplate = document.querySelector('template').content.querySelector('.map__pin');
 var mapListElement = document.querySelector('.map');
@@ -34,11 +67,11 @@ var getFeatures = function () {
 
 var addLeadingZeros = function (value, needingLenght) {
   var stringValue = '' + value;
-  while(stringValue.length < needingLenght) {
+  while (stringValue.length < needingLenght) {
     stringValue = '0' + stringValue;
   }
   return stringValue;
-}
+};
 
 var getAds = function () {
   var similarAds = [];
@@ -71,6 +104,8 @@ var getAds = function () {
   return similarAds;
 };
 
+var similarAds = getAds();
+
 mapToggle.classList.remove('map--faded');
 
 for (var i = 0; i < similarAds.length; i++) {
@@ -98,9 +133,9 @@ for (var i = 0; i < similarAds.length; i++) {
     photoTemplate.parentNode.appendChild(photoNext);
   }
   var featuresList = insertedMapListElement.querySelector('.popup__features');
-  for (var j = 0; j < AD_FEATURES.length; j++) {
-    if (similarAds[i].offer.features.indexOf(AD_FEATURES[j]) == -1) {
-      var deleteElement = featuresList.querySelector('.popup__feature--' + AD_FEATURES[j]);
+  for (var g = 0; g < AD_FEATURES.length; g++) {
+    if (similarAds[i].offer.features.indexOf(AD_FEATURES[g]) === -1) {
+      var deleteElement = featuresList.querySelector('.popup__feature--' + AD_FEATURES[g]);
       featuresList.removeChild(deleteElement);
     }
   }
